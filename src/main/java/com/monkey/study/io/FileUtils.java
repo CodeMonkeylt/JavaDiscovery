f26bc52aa22acb893980ea3ea56d0fe361073869
lt <taoliunancy@gmail.com>
2017-04-19 17:49:57 +0800

    功能初步完成

--- src/main/java/com/monkey/study/io/FileUtils.java
+++ src/main/java/com/monkey/study/io/FileUtils.java
@@ -21,18 +21,23 @@ public class FileUtils {
 			if (dir.isDirectory()) {
 				// print the Directory
 				//change directory name
-				if(dir.getName().contains(regex)){
-					dir.renameTo(new File(replacement));
+				if(dir.getName().matches(regex)){
+					dir.renameTo(new File(dir.getParent()+"\\"+replacement));
 				}
 				// get all files and arrays of directory
 				File[] files = dir.listFiles();
 				// Recursion traversal each child files
-				for (File file : files) {
-					getFiles(file,"","");
+				if(files != null){
+					for (File file : files) {
+						getFiles(file,regex,replacement);
+					}
 				}
 			}
 			// if is a file,print it's name and directory
 			else {
+				if(dir.getName().toString().contains(regex)){
+					renameFile(dir.getAbsolutePath(),dir.getName(),replacement+dir.getName(). substring(dir.getName().indexOf("."), dir.getName().length()));
+				}
 				readFile(dir,regex,replacement);
 			}
 		}
@@ -51,6 +56,7 @@ public class FileUtils {
 		BufferedReader fileReader = null;
 		StringBuffer stringBuffer = null;
 		String str = null;
+		boolean bool = false;
 		try {
 			fileReader = new BufferedReader(new FileReader(file));
 			stringBuffer = new StringBuffer();
@@ -59,9 +65,8 @@ public class FileUtils {
 				stringBuffer = stringBuffer.append(tempStr).append("\n");
 			}
 			str = stringBuffer.toString();
-			if(str != null && str.length() > 0){
-				str =  str.replaceAll(regex, replacement);
-			}
+			str =  str.replaceAll(regex, replacement);
+			writeFile(file.getAbsolutePath(),str);
 		}catch (IOException e) {
 			e.printStackTrace();
 		}finally{
@@ -70,7 +75,6 @@ public class FileUtils {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			writeFile(file.getName(),str);
 			stringBuffer.delete(0, stringBuffer.length());
 		}
 	}
@@ -103,4 +107,25 @@ public class FileUtils {
 		}
 	}
 	
+	 /**
+	  * @param path
+	  * @param oldname
+	  * @param newname
+	  */
+	 public static void renameFile(String path,String oldname,String newname){ 
+	        if(!oldname.equals(newname)){//新的文件名和以前文件名不同时,才有必要进行重命名 
+		            File oldfile=new File(path+"/"+oldname); 
+		            File newfile=new File(path+"/"+newname); 
+		            if(!oldfile.exists()){
+		                return;//重命名文件不存在
+		            }
+		            if(newfile.exists()){//若在该目录下已经有一个文件和新文件名相同，则不允许重命名 
+		                System.out.println(newname+"已经存在！"); 
+		            }else{ 
+		                oldfile.renameTo(newfile); 
+		            } 
+	        }else{
+	            System.out.println("新文件名和旧文件名相同...");
+	        }
+	    }
 }
--- src/main/java/com/monkey/study/io/changeWord.java
+++ src/main/java/com/monkey/study/io/changeWord.java
@@ -1,11 +1,16 @@
 package com.monkey.study.io;
 
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.monkey.study.io.FileUtils;
 
 /**
 * @ClassName: changeWord 
@@ -14,12 +19,17 @@ import java.io.Reader;
 * @date 2017年4月11日 上午9:40:40
  */
 public class changeWord {
-//	public final static String url = "F:\\git\\JavaDemo\\src\\com\\monkey\\study\\enumDiscovery\\enumDemoTest.java";
-	public final static String url = "E:\\study";
+//	public final static String url = "F:\\\\git\\\\JavaDemo\\\\src\\\\com\\\\monkey\\\\study\\\\enumDiscovery\\\\enumDemoTest.java";
+//	public final static String url = "C:\\Users\\Administrator\\Desktop\\Test";
+	public final static String url = "E:\\st_liu\\study";
 	
 	public static void main(String[] args) {
-		 System.out.println("开始替换，" + System.currentTimeMillis());
-		 FileUtils.changeWord(url, "thinkgem", "monkey");
-		 System.out.println("替换结束，" + System.currentTimeMillis());
+		 SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		 long startTime = System.currentTimeMillis();
+		 System.out.println("开始替换，" + sdf.format(new Date()));
+		 FileUtils.changeWord(url, "Jeesite", "Study");
+		 long endTime = System.currentTimeMillis();
+		 System.out.println("替换结束，" + sdf.format(new Date()));
+		 System.out.println("用时："+(endTime-startTime));
 	}
 }
